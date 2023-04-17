9ed54fade36d47ff9213b31ff7456250f0174477
William Overton <william.overton@humanitec.com>
2023-04-17 07:45:00 -0700

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-score-humanitec delta --env development --app demo --org="${HUMANITEC_ORG}" --token "${HUMANITEC_TOKEN}" --deploy
+score-humanitec delta --env development --app demo \
+  --org="${HUMANITEC_ORG}" --token "${HUMANITEC_TOKEN}" --deploy
